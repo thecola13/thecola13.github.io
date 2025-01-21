291610783e31969a6fcfcfd99c3ca93ef2341165
Cola <cola@gmail.com>
2025-01-21 11:44:08 +0100

    Added social media links in footer

--- assets/js/header-footer.js
+++ assets/js/header-footer.js
@@ -12,6 +12,33 @@ document.addEventListener('DOMContentLoaded', function () {
       .then(data => {
         document.getElementById('footer').innerHTML = data;
         document.getElementById('footer').style.display = 'block';
+
+        createSocials();
       });
   });
+
+  const socialLinks = [
+    { name: 'GitHub', url: 'https://github.com/thecola13', icon: 'bi bi-github' },
+    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/luca-colaci-438837223/', icon: 'bi bi-linkedin' },
+    { name: 'Instagram', url: 'https://www.instagram.com/the.cola/', icon: 'bi bi-instagram' }
+];
+
+function createSocials() {
+    const socialLinksContainer = document.getElementById('social-links');
+    if (!socialLinksContainer) {
+        console.log('Social links container not found');
+        return 0;
+    } else {
+        console.log('Social links container found');
+    };
+
+    socialLinks.forEach(link => {
+        console.log(`Adding ${link.name} link to social links container`);
+        socialLinksContainer.innerHTML += `
+            <a href="${link.url}" target="_blank" class="text-secondary">
+            <i class="${link.icon} fs-4"></i>
+            </a>`;
+    });
+    return 1;
+}
   
